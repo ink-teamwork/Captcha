2ba33e93b35b176d2b883966b94084ebca28a390
zhaokai <985144332@qq.com>
2015-12-24 10:26:04 +0800

    Initial Commit

---
+++ Captcha/Captcha.h
@@ -0,0 +1,19 @@
+//
+//  Captcha.h
+//  Captcha
+//
+//  Created by code on 15/12/24.
+//  Copyright © 2015年 赵凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Captcha.
+FOUNDATION_EXPORT double CaptchaVersionNumber;
+
+//! Project version string for Captcha.
+FOUNDATION_EXPORT const unsigned char CaptchaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Captcha/PublicHeader.h>
+
+
